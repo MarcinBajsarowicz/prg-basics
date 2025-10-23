63597176c91d3cbcb782778a8568ee6b1db4e692
MarcinBajsarowicz <bajsarowiczmarcin@gmail.com>
2025-10-23 11:12:51 +0200

    esea

---
+++ 02-TypesAndVariables/e.g.3-7.py
@@ -0,0 +1,5 @@
+x = 7
+y = 34
+y = x
+
+print("Before swapping x=", x, "y=", y)
---
+++ 02-TypesAndVariables/second.py
@@ -0,0 +1,8 @@
+number1 = 21
+number2 = 69
+number3 = 67
+result = number1 + number2 + number3
+print('Number 1:', number1)
+print('Number 2:', number2)
+print('Number 3:', number3)
+print('The result of summation: ', result)
