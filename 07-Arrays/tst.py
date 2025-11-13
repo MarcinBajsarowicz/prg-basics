3a89bfe7266870c718b43098f79b206f67a39183
MarcinBajsarowicz <bajsarowiczmarcin@gmail.com>
2025-11-13 11:12:49 +0100

    esiea

---
+++ 07-Arrays/tst.py
@@ -0,0 +1,18 @@
+grade1 = 3.5
+grade2 = 4.0
+grade3 = 5.0
+grade4 = 3.5
+grade5 = 4.0
+
+grades = [3.5, 4.0, 5.0, 3.5, 4.0]
+print(grades[2])
+
+grades[2] = 2.0 
+
+grades[0]
+values = len(grades)
+
+
+grades[len(grades)-1]
+
+gardes[int(len(grades)/2)]
