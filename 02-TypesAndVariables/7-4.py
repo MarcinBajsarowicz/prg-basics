359d6588a828a19800be9506ca4684dc8542643d
MarcinBajsarowicz <bajsarowiczmarcin@gmail.com>
2025-11-16 21:45:51 +0100

    recebka

---
+++ 02-TypesAndVariables/7-4.py
@@ -0,0 +1,5 @@
+minial_tree = 157
+tree = float(input("Enter tree circumference in cm: "))
+
+cut_down = minial_tree > tree
+print(f"Tree can be cut down: {cut_down}")
---
+++ 02-TypesAndVariables/7-6.py
@@ -0,0 +1,3 @@
+speed = int(input("Enter your speed: "))
+speed_limit = 140 >= speed >= 40
+print(f"Speed is valid: {speed_limit}")
