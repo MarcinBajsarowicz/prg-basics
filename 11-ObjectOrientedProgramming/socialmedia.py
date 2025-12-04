cbb92c6e3e7cc246cd132043d69a6ac70604f701
MarcinBajsarowicz <bajsarowiczmarcin@gmail.com>
2025-12-04 11:05:27 +0100

    ssss

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,20 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def display_timeline(self):
+        for index, post in enumerate(self.posts, start = 1):
+            print(f'{index}. {post}')
+    
+def main():
+    johndoe = SocialMediaProfile("johndoe")
+
+    johndoe.add_post("Hello World!")
+    johndoe.add_post("Had a great time at park")
+    johndoe.add_post("Whats up Nataie how are you?")
+
+    johndoe.display_timeline()
+
+if __name__ == "__main__":
+    main()
+
+
